504c6f56dbd5de98c58724705aa977d9e67e6f0e
YogeshRajan66 <yogeshr0510@gmail.com>
2026-06-26 16:57:53 +0530

    Project 5:LoadMoreData Component Completed

--- React_Projects/src/App.jsx
+++ React_Projects/src/App.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import Accordian from './components/accordian'
-import RandomColor from './components/random-color-generator'
-import StarRating from './components/star-rating'
-import ImageSlider from './components/image-slider'
-import LoadMoreData from './components/load-more-data'
+import React from "react";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color-generator";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 
 const App = () => {
   return (
     <center>
-      <div className='app'>
-    {/* <Accordian/>
+      <div className="app">
+        {/* <Accordian/>
     <RandomColor/> */}
-    {/* <StarRating/> */}
-    {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'}limit={'10'}/> */}
-    <LoadMoreData/>
-    </div>
+        {/* <StarRating/> */}
+        {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'}limit={'10'}/> */}
+        <LoadMoreData />
+      </div>
     </center>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- React_Projects/src/components/load-more-data/index.jsx
+++ React_Projects/src/components/load-more-data/index.jsx
@@ -1,23 +1,57 @@
 import { useEffect, useState } from "react";
-
+import "./styles.css";
 export default function LoadMoreData() {
   const [loading, setloading] = useState(false);
   const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
-  async function fetchProducts(utl) {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [disableButton, setDisableButton] = useState(false);
+  async function fetchProducts() {
     try {
       setloading(true);
       const response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`,
+        `https://dummyjson.com/products?limit=20&skip=${count * 20}`,
       );
-      const data = response.json();
-      console.log(data)
+      const data = await response.json();
+      if (data.products.length > 0) {
+        setProducts((prev) => [...prev, ...data.products]);
+        setloading(false);
+      } else {
+        setDisableButton(true);
+      }
+      console.log(data.products);
     } catch (e) {
-        console.log(e)
+      console.log(e);
+      setErrorMsg(e.message);
+      setloading(false);
+    } finally {
+      setloading(false);
     }
   }
   useEffect(() => {
     fetchProducts();
-  }, []);
-  return <div className="container"></div>;
+  }, [count]);
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  if (errorMsg !== null) return <div>Error Occured:{errorMsg}</div>;
+  return (
+    <div className="load-more-container">
+      <div className="products-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="products" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+      </div>
+    </div>
+  );
 }
